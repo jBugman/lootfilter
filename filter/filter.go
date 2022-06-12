17b782f53391e5e10c0f0424271d211b9016c30c
Sergey Parshukov <codedby@bugman.me>
2022-06-12 22:40:50 +0400

    Add chromatic recipe

--- filter/filter.go
+++ filter/filter.go
@@ -9,39 +9,92 @@ const defaltFontSize = 34
 
 type Filter struct {
 	sections []section
+
+	Chromatic bool
+}
+
+func (f Filter) virtualSections() []section {
+	ss := f.sections
+	if f.Chromatic {
+		ss = append(ss,
+			section{
+				Visibility:  Show,
+				SocketGroup: "RGB",
+				Height:      3,
+				Width:       1,
+				BorderColor: "235 235 235 255",
+			},
+			section{
+				Visibility:  Show,
+				SocketGroup: "RGB",
+				Height:      2,
+				Width:       2,
+				BorderColor: "235 235 235 255",
+			},
+		)
+	}
+	return ss
 }
 
 func (f Filter) String() string {
 	var buf strings.Builder
-	for _, sec := range f.sections {
-		sectionTemplate.Execute(&buf, sec)
+	for _, sec := range f.virtualSections() {
+		_, _ = buf.WriteString(sec.String())
+		_, _ = buf.WriteString("\n")
 	}
-	return buf.String()
+	res := strings.TrimSpace(buf.String())
+	res += "\n"
+	return res
 }
 
-var Default = Filter{
+var defaultFilter = Filter{
 	sections: []section{
 		{
-			Visibility: show,
+			Visibility: Show,
 			FontSize:   34,
 		},
 	},
+	Chromatic: true,
 }
+
+var Default = defaultFilter
 
 type visibility string
 
-const hide visibility = "Hide"
-const show visibility = "Show"
+const Hide visibility = "Hide"
+const Show visibility = "Show"
 
 type section struct {
 	Visibility visibility
-	FontSize   int
+
+	SocketGroup string
+
+	Height int
+	Width  int
+
+	FontSize    int
+	BorderColor string
+}
+
+func (sec section) String() string {
+	var buf strings.Builder
+	sectionTemplate.Execute(&buf, sec)
+	return buf.String()
 }
 
 var sectionTemplate template.Template
 
 func init() {
 	sectionTemplate = *template.Must(template.New("section").Parse(`{{ .Visibility }}
-	{{ if .FontSize -}}SetFontSize {{ .FontSize }}{{- end }}
+	{{- if .SocketGroup }}
+	SocketGroup "{{ .SocketGroup }}"{{- end }}
+	{{- if .Height }}
+	Height <= {{ .Height }}{{- end }}
+	{{- if .Width }}
+	Width <= {{ .Width }}{{- end }}
+	{{- if .FontSize }}
+	SetFontSize {{ .FontSize }}{{- end }}
+	{{- if .BorderColor }}
+	SetBorderColor {{ .BorderColor }}{{- end }}
 `))
 }
