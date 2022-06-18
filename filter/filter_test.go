727777e26a778bf71d4858fe744d874a8041fe78
Sergey Parshukov <codedby@bugman.me>
2022-06-18 18:10:27 +0400

    Hex color helper

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -19,3 +19,8 @@ func TestClassesString(t *testing.T) {
 		(Classes{"Helmets", "Amulet"}).String(),
 	)
 }
+
+func TestColorConversion(t *testing.T) {
+	require.EqualValues(t, hex("fff"), "255 255 255 255")
+	require.EqualValues(t, hex("#1F54BF"), "31 84 191 255")
+}
