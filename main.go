dc3f212ba2d02a509568c581c88f66b5f4bcdc63
Sergey Parshukov <codedby@bugman.me>
2022-06-18 16:10:34 +0400

    cmd flags

--- main.go
+++ main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/user"
-	"path"
+	"path/filepath"
 
 	"lootfilter/filter"
 )
 
 func main() {
-	osUser, err := user.Current()
-	if err != nil {
-		fatal(err)
-	}
+	var outName string
+	var outPath string
 
-	home := osUser.HomeDir
-	poeFolder := path.Join(home, "Documents", "My Games", "Path of Exile")
+	flag.StringVar(&outName, "name", "generated", "Filter file name")
+	flag.StringVar(&outPath, "path", defaultPath(), "Filter location")
+	flag.Parse()
 
-	filterFile := path.Join(poeFolder, "generated.filter")
+	filterFile := filepath.Join(outPath, outName+".filter")
 
 	custom := filter.Default
 
 	if err := os.WriteFile(filterFile, []byte(filter.Render(custom)), 0o644); err != nil {
 		fatal(err)
 	}
+}
+
+func defaultPath() string {
+	osUser, err := user.Current()
+	if err != nil {
+		fatal(err)
+	}
+
+	return filepath.Join(osUser.HomeDir, "Documents", "My Games", "Path of Exile")
 }
 
 func fatal(err error) {
